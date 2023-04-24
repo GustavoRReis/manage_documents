4f2d0e77b423ca9fda382867f170d7eb07e77d80
gustavo ramon dos reis <gustavobilireis@hotmail.com>
2023-04-24 14:27:23 -0300

    feat: tela de cadastro de novo usuario

--- managedocuments/src/App.js
+++ managedocuments/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import EmployeeForm from './components/EmployeeForm/EmployeeForm'
 import Login from './pages/Login/Login'
+import Register from './pages/Register/Register';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
         <Route exact path="/employee" component={EmployeeForm} />
       </Switch>
     </BrowserRouter>
---
+++ managedocuments/src/components/CardPdf/CardPdf.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CardPdf() {
+  return (
+    <div>CardPdf</div>
+  )
+}
